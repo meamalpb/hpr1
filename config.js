9b83a5d70c36ee6017ab9ca1b8640cba6e61ddd8
unknown <321amalpb.com@gmail.com>
2021-05-20 22:16:36 +0530

    firebase connect

---
+++ config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB-bxU-jIijwf2dVYAIDDOR4-Pg-38PNeM",
+    authDomain: "blog-a4e41.firebaseapp.com",
+    projectId: "blog-a4e41",
+    storageBucket: "blog-a4e41.appspot.com",
+    messagingSenderId: "684909501088",
+    appId: "1:684909501088:web:c3b7722759446ecfc507c5"
+  };
+
+  module.exports = firebaseConfig;
