6b1c5026a8ab3d795754a97744bb451c0c5d30f8
Midhun Chandran <midhun.chandran529@gmail.com>
2021-05-22 16:02:05 +0530

    blogs page

--- blogs/data.js
+++ blogs/data.js
@@ -1,19 +1,20 @@
-const listOfBlogs = document.querySelector('.blogcontent');
+const listOfBlogs = document.querySelector('.collapsible');
 
 db.collection('blogs').get().then(snapshot => {
    
    
-    
+    // for fire base
     let html = '';
     const data=snapshot.docs;
    
        data.forEach(doc => {
       const blog = doc.data();
       const li = `
-        <li>
-        <button type="button" class="collapsible" ><h3>${blog.title}</h3></button>
-        <div class="content">
-          <p>${blog.content}</p>
+        <li><div class="collapsible-header #ef5350 red lighten-1">
+        ${blog.title}
+        </div>
+        <div class="collapsible-body  #ef9a9a red lighten-3">
+          <span>${blog.content}</span>
         </div>
         </li>
       `;
@@ -22,4 +23,10 @@ db.collection('blogs').get().then(snapshot => {
     listOfBlogs.innerHTML = html;
     
   
+  });
+
+ //for collapsble blogs 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
   });
