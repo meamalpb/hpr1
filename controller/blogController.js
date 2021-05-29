eb4ff5b4f54289395470ef73e0de463284ffcec8
unknown <321amalpb.com@gmail.com>
2021-05-29 22:34:09 +0530

    minor changes

--- controller/blogController.js
+++ controller/blogController.js
@@ -4,7 +4,7 @@ const firebase = require('firebase');
 
 module.exports.postblogs=async(req,res)=>{
     console.log(req.body)
-    firebase.firestore().collection('riz_blogs').doc('aa').set({title:req.body.title,content:req.body.content}).then((res)=>{console.log(res)})
+    firebase.firestore().collection('riz_blogs').add({title:req.body.title,content:req.body.content}).then((res)=>{console.log(res)})
 }
 
 module.exports.get_blog = (req,res) => {
