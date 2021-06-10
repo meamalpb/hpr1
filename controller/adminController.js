1897880855dd177085bc9c1e0fd60240fb0c7c85
unknown <321amalpb.com@gmail.com>
2021-06-10 21:21:51 +0530

    otp function

--- controller/adminController.js
+++ controller/adminController.js
@@ -100,3 +100,12 @@ module.exports.changeusertype=async(req,res)=>{
 }
 
 
+generatePassword = ()=> {
+  var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
